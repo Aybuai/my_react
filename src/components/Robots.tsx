5d3fae5e9152a8f8fdb191c9b5127b775d115d81
Aybuai <13624448268@163.com>
2023-07-23 12:43:55 +0800

    fix： 修改代码格式

--- src/components/Robots.tsx
+++ src/components/Robots.tsx
@@ -1,17 +1,22 @@
-import React from 'react';
+import React from "react";
 
 interface RobotProps {
-    id: number,
-    name: string,
-    email: string
+  id: number;
+  name: string;
+  email: string;
 }
 
-const Robot : React.FC<RobotProps> = ({ id, name, email }) => {
-    return <li>
-        <img src={ `https://robohash.org/%E9%98%BF%E8%8E%B1%E5%85%8B%E6%96%AF?set=set${id}` } alt="robot" />
-        <h2>{name}</h2>
-        <span>{email}</span>
+const Robot: React.FC<RobotProps> = ({ id, name, email }) => {
+  return (
+    <li>
+      <img
+        src={`https://robohash.org/%E9%98%BF%E8%8E%B1%E5%85%8B%E6%96%AF?set=set${id}`}
+        alt="robot"
+      />
+      <h2>{name}</h2>
+      <span>{email}</span>
     </li>
-}
+  );
+};
 
-export default  Robot
+export default Robot;
