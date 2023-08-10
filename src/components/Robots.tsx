39f58ed19395dbb54ab2edbe0973b6712eac48d7
Aybuai <13624448268@163.com>
2023-08-10 16:08:03 +0800

    feat: 增加css模块化 & css类型推导

--- src/components/Robots.tsx
+++ src/components/Robots.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "./Robots.module.css";
 
 interface RobotProps {
   id: number;
@@ -8,14 +9,14 @@ interface RobotProps {
 
 const Robot: React.FC<RobotProps> = ({ id, name, email }) => {
   return (
-    <li>
+    <div className={styles.cardContainer}>
       <img
         src={`https://robohash.org/%E9%98%BF%E8%8E%B1%E5%85%8B%E6%96%AF?set=set${id}`}
         alt="robot"
       />
       <h2>{name}</h2>
       <span>{email}</span>
-    </li>
+    </div>
   );
 };
 
