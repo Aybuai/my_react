a47751e1efcb46b7572ccdee57dcf6ac877b6aa1
Aybuai <13624448268@163.com>
2023-08-31 10:35:39 +0800

    feat: add font and image assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import logo from "./assets/images/logo.svg";
 import robots from "./mockData/robots.json";
 import Robots from "./components/Robots";
 import styles from "./App.module.css";
@@ -6,6 +7,10 @@ import styles from "./App.module.css";
 function App() {
   return (
     <div className={styles.app}>
+      <div className={styles.appHeader}>
+        <img src={logo} className={styles.appLogo} alt="logo" />
+        <h1>罗伯特机器人吊炸天平台</h1>
+      </div>
       <div className={styles.robotList}>
         {robots.map((r) => (
           <Robots id={r.id} name={r.name} email={r.email} />
