0b98999dccd26f1014faf78d212dcbf2f0255c08
Aybuai <13624448268@163.com>
2023-08-31 11:06:14 +0800

    feat: add shopping cart component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import logo from "./assets/images/logo.svg";
 import robots from "./mockData/robots.json";
 import Robots from "./components/Robots";
 import styles from "./App.module.css";
+import ShoppingCart from "./components/ShoppingCart";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <img src={logo} className={styles.appLogo} alt="logo" />
         <h1>罗伯特机器人吊炸天平台</h1>
       </div>
+      <ShoppingCart />
       <div className={styles.robotList}>
         {robots.map((r) => (
           <Robots id={r.id} name={r.name} email={r.email} />
